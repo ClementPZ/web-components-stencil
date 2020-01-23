e8b92964df68c4d50817290ee1539b1c1078576c
clementpicardzivy <clementpicardzivy@gmail.com>
2020-01-23 15:08:43 -0600

    first componenent created BUT render() not rendering in browser, el in DOM but issue with render method

--- src/components/my-component/side-drawer/side-drawer.tsx
+++ src/components/my-component/side-drawer/side-drawer.tsx
@@ -1,6 +1,15 @@
 import { Component } from "@stencil/core";
+import { h } from "@stencil/core";
 
-@Component()
-class SideDrawer {
-
+@Component({
+    tag: 'my-side-drawer'
+})
+export class SideDrawer {
+    render() {
+        return (
+            <div>
+                <h1>hello from my side drawer</h1>
+            </div>
+        );
+    }
 }
